fd9f2ccfada7f01500fb1816fb15d3f2eeb7cac1
Ksenia Tishchenko <k.tishchenkox@gmail.com>
2022-06-04 16:32:23 +0400

    make account card border-radius match the amount card

--- src/entities/AccountCard/ui/AccountCard.tsx
+++ src/entities/AccountCard/ui/AccountCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const AccountCard: FC<Props> = ({ item: { amounts, title } }) => {
     return (
-        <div className="mb-4 p-4  mr-4 basis-56 rounded shadow">
+        <div className="mb-4 p-4  mr-4 basis-56 rounded-md shadow">
             <h2 className="font-medium text-xl pb-2">{title}</h2>
             {amounts.map((amount, index) => (
                 <AmountCard amount={amount} key={index} />
