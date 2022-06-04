6c16530df82c20d20be5bd20b0f0fc49ab8fc866
Ksenia Tishchenko <k.tishchenkox@gmail.com>
2022-06-04 14:22:54 +0400

    change AccountCard visuals

--- src/entities/AccountCard/ui/AmountCard.tsx
+++ src/entities/AccountCard/ui/AmountCard.tsx
@@ -8,9 +8,16 @@ interface Props {
 
 export const AmountCard: FC<Props> = ({ amount: { amount, currency } }) => {
     return (
-        <>
-            {amount}{' '}
-            <span className="font-medium">{mapCurrency(currency)}</span>
-        </>
+        <div
+            className="border border-gray-300 rounded-lg
+                        py-1 px-3
+                        mr-1
+                        inline-block
+                        font-thin
+                        bg-gray-100
+                        text-slate-800"
+        >
+            {amount} <span>{mapCurrency(currency)}</span>
+        </div>
     );
 };
